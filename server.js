a70788b6220413eccd66680cad47010642f069d6
avin1508 <omkumar55800@gmail.com>
2025-09-02 23:45:40 +0530

    added sucessfully

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from "public" folder (automatically serves index.html)
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
